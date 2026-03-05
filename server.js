d989166a2f02d3ba34f10b6c1e8519794963a438
Vipash <vipashmeena@gmail.com>
2026-03-06 03:05:25 +0530

    abort mission... revert to default settings

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const Alumni = require('./Alumni'); 
+const Alumni = require('./alumni'); 
 const path = require('path');
 
 const app = express();
