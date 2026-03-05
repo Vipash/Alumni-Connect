bed6ea593f9320d9910095e70d6a920b0395afb7
Vipash <vipashmeena@gmail.com>
2026-03-06 02:10:54 +0530

    Cleanup: Remove duplicated alumni-backend folder

--- alumni-backend/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['junior', 'alumni', 'admin'], default: 'junior' }
-});
-
-module.exports = mongoose.model('User', UserSchema);
