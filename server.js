4cfb8bb3f74eb32a7616c9671c350d62addd62a0
Vipash <vipashmeena@gmail.com>
2026-03-06 03:43:29 +0530

    more fixing

--- server.js
+++ server.js
@@ -63,9 +63,14 @@ app.patch('/api/approve-alumni/:id', async (req, res) => {
 
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+  app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res, next) => {
+    if (req.path.startsWith('/api')) {
+        return next();
+    }
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+} 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
