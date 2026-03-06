928b672dfe7136afc62626d2305ab08dfdfce6c4
Vipash <vipashmeena@gmail.com>
2026-03-07 02:39:52 +0530

    revamp

--- src/LocationPicker.jsx
+++ src/LocationPicker.jsx
@@ -3,12 +3,11 @@ import { useMapEvents } from 'react-leaflet';
 function LocationPicker({ setCoords }) {
   useMapEvents({
     click(e) {
-      // e.latlng contains the [lat, lng] of where the user clicked
+      // e.latlng contains the latitude and longitude of where you clicked
       setCoords([e.latlng.lat, e.latlng.lng]);
     },
   });
-  return null;
+  return null; // This component doesn't "render" anything visual, it just listens for clicks
 }
 
-// THIS IS THE MISSING LINE:
 export default LocationPicker;
