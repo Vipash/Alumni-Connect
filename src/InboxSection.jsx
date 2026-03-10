b657d94c1ae065f56e24357cc22e642daca4a51c
Vipash <vipashmeena@gmail.com>
2026-03-11 02:54:50 +0530

    yo phon linging

---
+++ src/InboxSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function InboxSection() {
+  return (
+    <div className="tab-pane">
+      <div className="placeholder-content">
+        <h2>Inbox</h2>
+        <p>Your notifications and messages will appear here soon.</p>
+        <div className="placeholder-icon">📩</div>
+      </div>
+    </div>
+  );
+}
+
+export default InboxSection;
